ae3f278b182217f206fa111680495e66d989c1e4
Mitchman215 <mitchell_salomon@brown.edu>
2023-09-15 14:08:24 -0400

    deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // https://docs.astro.build/en/guides/deploy/github/
+  // TODO: change site to brownclimbingclub.com and remove base once deployed
+  site: 'https://brownclimbing.github.io/', 
+  base: '/brownclimbingclub.com'
+});
